62d9c8626000a1347c05f7762c37f7b54e2572e3
MOREL Rodrigue <morel.rodrigue@protonmail.com>
2021-11-14 23:35:47 +0100

    functions calculate

--- functions.sh
+++ functions.sh
@@ -5,12 +5,13 @@ function calculate {
   local b=$2
   local c=$3
 
-  if [ -z $a ]
+  if [ -z $a ] || [ -z $b ] || [ -z $c ]
   then
     echo "Veuillez spécifier les paramètres comme ceci (premier chiffre, le signe puis le deuxième chiffre) : 10 * 2 ou 9 + 4"
+    exit 1;
+  else
+    result=$(($a$b$c))
   fi
-
-  result=$(($a$b$c))
 }
 
 calculate $1 $2 $3
